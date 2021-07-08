8870f51e9a891bb2b19f97b55bb51edc0a88a070
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-07-09 02:37:36 +0900

    add more style for form

--- client/src/components/common/StockInsert/styles.ts
+++ client/src/components/common/StockInsert/styles.ts
@@ -2,14 +2,30 @@ import styled from "styled-components";
 
 export const InsertForm = styled.form`
   display: flex;
+  outline: none;
+  margin: 2rem;
+  width: 90%;
+  justify-content: center;
 `;
 
 export const InsertInput = styled.input`
   background: none;
   outline: none;
-  padding: 0.5rem;
+  padding: 1rem;
+  border: solid 1px #b0b0b0;
+  width: 20rem;
 `;
 
 export const AddButton = styled.button`
-  color: red;
+  background: #0081f2;
+  color: white;
+  outline: none;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: 1rem;
+  border: none;
+
+  &:hover {
+    background: #0f55de;
+  }
 `;
