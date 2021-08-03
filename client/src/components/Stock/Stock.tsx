a31453b92d836c61984a6e6bb872154fc4e30dcc
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-08-04 03:17:13 +0900

    [Feature] : Adding a simple barchart

--- client/src/components/Stock/Stock.tsx
+++ client/src/components/Stock/Stock.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "../common/Header/Header";
+import BarChart from "../Chart/BarChart";
 
 const Stock = () => {
   const { ticker }: any = useParams();
@@ -17,9 +18,16 @@ const Stock = () => {
       });
   }, [ticker]);
 
-  console.log(stockDetails);
+  // console.log(stockDetails);
 
-  const { name, ticker: stockTicker, sector }: any = stockDetails || {};
+  const {
+    name,
+    ticker: stockTicker,
+    sector,
+    divHistory,
+  }: any = stockDetails || {};
+
+  console.log(divHistory);
 
   return (
     <div>
@@ -28,6 +36,7 @@ const Stock = () => {
         <h1>{name}</h1>
         <small>{`Stock Symbol: ${stockTicker}`}</small>
         <h3> {sector} </h3>
+        <BarChart />
       </div>
     </div>
   );
