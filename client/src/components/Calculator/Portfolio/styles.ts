a4a19dd716a019f39409a3714b3762d1f06d4783
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-08-14 01:19:41 +0900

    each stock styling

--- client/src/components/Calculator/Portfolio/styles.ts
+++ client/src/components/Calculator/Portfolio/styles.ts
@@ -4,10 +4,10 @@ export const StockItem = styled.div`
   display: flex;
   box-shadow: 0 1px 0 0 #e3edee !important;
 
-  background-color: #e1eff9;
+  padding: 10px 15px;
+  background-color: #fff;
   border-radius: 8px;
   margin-bottom: 15px;
-  display: flex;
   transition: 0.4s;
   width: max-content;
   width: 100%;
