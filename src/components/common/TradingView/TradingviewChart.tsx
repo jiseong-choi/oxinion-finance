3c81fccb09209b140cf1fdd3c8b1316757d0c0f8
Brandon Ha <idevbrandon@gmail.com>
2021-10-30 20:02:46 +0900

    [ADD] : Display a chart

--- src/components/common/TradingView/TradingviewChart.tsx
+++ src/components/common/TradingView/TradingviewChart.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { AdvancedChart } from "react-tradingview-embed";
 
-const TradingviewChart = () => {
-  return <div>chart</div>;
-};
+export default function TradingviewChart() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <AdvancedChart
+        widgetProps={{
+          width: 980,
+          height: 610,
+          symbol: "aapl",
+          interval: "W",
+          timezone: "Europe/Rome",
+          theme: "dark",
+          style: "8",
+          locale: "en",
+          toolbar_bg: "#f1f3f6",
+          enable_publishing: false,
+          withdateranges: true,
+          save_image: false,
 
-export default TradingviewChart;
+          container_id: "tradingview_929ec",
+        }}
+      />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
